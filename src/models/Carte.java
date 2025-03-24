57f50ab5b41a582e8cdbb60698f62375972e4eef
Kmilleee <camillepayen35@gmail.com>
2025-03-24 09:52:28 +0100

    Carte.java

--- src/models/Carte.java
+++ src/models/Carte.java
@@ -1,6 +1,34 @@
-package models;
+
 
 class Carte {
+  private String nom;
+  private String prenom;
+  private String typeContrat;
+  private float soldeForfait;
 
-    
+  public Carte(String nom, String prenom, String typeContrat, float soldeForfait) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.typeContrat = typeContrat;
+    this.soldeForfait = soldeForfait;
+  }
+
+  public boolean estUnForfait() {
+    return typeContrat.equals("Forfait");
+  }
+
+  public float lireSolde() {
+    return soldeForfait;
+  }
+
+  public void decompterCharge(float montant) {
+    if (!estUnForfait()) {
+        throw new UnsupportedOperationException("Impossible de décompter un montant sur un abonnement.");
+    }
+    if (montant > soldeForfait) {
+        throw new IllegalArgumentException("Solde insuffisant pour cette charge.");
+    }
+    soldeForfait -= montant;
+}
+   
 }
