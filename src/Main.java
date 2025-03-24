d51f470d85a50d647b55dbf76c0dfa2a40e51fa5
KyleAktr <aktr.contact@gmail.com>
2025-03-24 17:27:56 +0100

    swing added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,57 @@
+import java.util.Scanner;
+
+import models.Borne;
+import models.Carte;
+import models.TypeRecharge;
+import utils.ClearScreen;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        // Création d'une carte
+        Carte carte = new Carte("Dupont", "Jean", "Forfait", 100.0f);
+
+        // Création d'un type de recharge
+        TypeRecharge typeRecharge = new TypeRecharge("Rapide", 50);
+
+        // Création d'une borne
+        Borne borne = new Borne(1, typeRecharge);
+
+        borne.insererCarte(carte);
+        Scanner scanner = new Scanner(System.in);
+        int choix;
+
+        do {
+
+            System.out.println("Menu :");
+            System.out.println("1. Afficher les informations de la borne et de la carte");
+            System.out.println("2. Insérer la prise");
+            System.out.println("3. Démarrer le chargement");
+            System.out.println("4. Quitter");
+            choix = scanner.nextInt();
+            
+            switch (choix) {
+                case 1:
+                ClearScreen.clearScreen();
+                borne.afficher();
+                break;
+                case 2:
+                ClearScreen.clearScreen();
+                borne.insererPrise();
+                break;
+                case 3:
+                ClearScreen.clearScreen();
+                borne.charger();
+                break;      
+                case 4:
+                ClearScreen.clearScreen();
+                break;
+                
+                default:
+                ClearScreen.clearScreen();
+                System.out.println("Saisie incorrecte");
+                break;
+            }
+        } while (choix != 4);
+        scanner.close();
+    }
 }
--- src/models/Borne.java
+++ src/models/Borne.java
@@ -1,8 +1,49 @@
 package models;
 
-class Borne {
+public class Borne {
+
+    private int id;
+    private TypeRecharge typeRecharge;
+    private Carte carte;
+
+    public Borne(int id, TypeRecharge typeRecharge) {
+        this.id = id;
+        this.typeRecharge = typeRecharge;
+    }
+
+    public void insererCarte(Carte carte) {
+        if (carte == null) {
+            System.out.println("Carte non insérée");
+            return;
+        }
+        else {
+            this.carte = carte;
+            System.out.println("Carte insérée");
+        }
+    }
+
+    public String afficher() {
+        StringBuilder info = new StringBuilder();
+        info.append("Borne{")
+            .append("id=").append(id)
+            .append(", typeRecharge=").append(typeRecharge)
+            .append("}\n");
+        info.append("Infos de la carte :\n")
+            .append("nom='").append(carte.getNom()).append("'\n")
+            .append("prenom='").append(carte.getPrenom()).append("'\n")
+            .append("typeContrat='").append(carte.getTypeContrat()).append("'\n")
+            .append("soldeForfait=").append(carte.getSoldeForfait());
+        return info.toString();
+    }
+
+    public void insererPrise() {
+        System.out.println("Prise insérée");
+    }
+
+    public void charger() {
+        System.out.println("Chargement en cours");
+    }
+    
 
 
-    
-    
 }
