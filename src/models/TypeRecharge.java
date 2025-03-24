3f97cc5457cda5b1434639060928ca345b91e1a1
KyleAktr <aktr.contact@gmail.com>
2025-03-24 09:20:16 +0100

    TypeRecharge class

--- src/models/TypeRecharge.java
+++ src/models/TypeRecharge.java
@@ -1,3 +1,28 @@
+package models;
+
 public class TypeRecharge {
-  
+
+    private String nom; 
+    private int puissance; 
+
+    public TypeRecharge(String nom, int puissance) {
+        this.nom = nom;
+        this.puissance = puissance;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPuissance() {
+        return puissance;
+    }
+
+    @Override
+    public String toString() {
+        return "TypeRecharge{" +
+                "nom='" + nom + '\'' +
+                ", puissance=" + puissance + " kW" +
+                '}';
+    }
 }
